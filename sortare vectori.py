cdde9dcfd2b64dd11065e9b565e87e8e7a18cab3
George Valentin Casiu <casrom@gmail.com>
2023-02-06 12:04:04 +0200

    Add files via upload

---
+++ sortare vectori.py
@@ -0,0 +1,69 @@
+# Realizat de /Produced by Casiu George Valentin
+"""
+Diverse tipuri de sortare a vectorilor/Varios type of vector sorting
+In utile_vectori.py avem proceduri de citirea si afisarea vectorilor de la consola cat si din fisier/
+In utile_vectori we have procedures to read and display vectors from the console and the file.
+"""
+
+import utile_vectori as uv
+
+n,v=uv.citeste_f("date.in","it")
+print("V initial: ")
+print(v)
+uv.afisare(v)
+
+"""
+#sortare prin minim/sort by minimum
+for i in range (n-1):
+    min=v[i]
+    k=i
+    for j in range(i+1,n):
+        if v[j]<min:
+            k,min=j,v[j]
+    v[i],v[k]=v[k],v[i]
+print("V sortat: ")
+uv.afisare(v)
+"""
+
+"""
+#sortarea prin interschimbare(Bubble Sort)/sorting by swap(Bubble Sort)
+rez=True
+while rez:
+    rez=False
+    for i in range (n-1):
+        if v[i]>v[i+1]:
+            v[i],v[i+1]=v[i+1],v[i]
+            rez=True
+print("V sortat: ")
+uv.afisare(v)
+"""
+
+"""
+#sortarea prin insertie/insertion sort
+a=[0 for i in range(n)]
+a[0]=v[0]
+for i in range (1,n):
+    j=i-1
+    while v[i]<a[j] and j>=0:
+        a[j+1]=a[j]
+        j-=1
+    a[j+1]=v[i]
+print("V sortat: ")
+uv.afisare(a)
+"""
+
+"""
+#sortare cu sort/sort by "sort"
+v.sort(reverse=False)
+print("V sortat: ")
+uv.afisare(v)
+"""
+
+"""
+#sortare cu sorted/sort by "sorted"
+a=sorted(v, reverse=False)
+print("V initial dupa sortare: ") #v ramane neschimbat
+uv.afisare(v)
+print("V sortat: ")
+uv.afisare(a)
+"""
---
+++ utile_vectori.py
@@ -0,0 +1,33 @@
+def citire(n,v,tip):
+    for i in range (n):
+        if tip=="int":
+            v.append(int(input("V["+str(i)+"]=")))
+        elif tip=="float":
+            v.append(float(input("V["+str(i)+"]=")))
+        else:
+            v.append(input("V["+str(i)+"]="))
+
+def citeste_f(fisier,tip):
+    with open(fisier,"r") as f:
+        n=int(f.readline())
+        linie=f.readline() #sau se poate folosi/or you can use directli: linie=f.readline().replace("\n"," ").split()
+        linie=linie.replace("\n"," ")
+        linie=linie.split()
+        if tip=="int":
+            return [n,[int(el) for el in linie]]
+        elif tip=="float":
+            return [n,[float(el) for el in linie]]
+        else:
+            return[n,linie]
+
+def afisare(v):
+    for el in v:
+        print(el,end=" ")
+    print()
+
+def scrie_f(fisier,v):
+    with open (fisier, "w") as f:
+        for el in v:
+            f.write(str(el)+" ")
+        f.write("\n")
+
